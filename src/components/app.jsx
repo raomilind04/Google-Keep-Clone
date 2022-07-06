aab446424a5588c7904f26502d7b14515b166880
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-07-06 22:14:44 +0530

    add props and components for note element

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./header"; 
 import Footer from "./footer"; 
+import Note from "./note"; 
+import notes from "../notes"; 
+
+function createNotes(note){
+    return (
+        <Note 
+            key= {note.key}
+            title= {note.title}
+            content= {note.content}
+        />
+    )
+}
 
 function App(){
     return (
         <div>
             <Header/>
+            {notes.map(createNotes)}; 
             <Footer/>
         </div>
     ); 
